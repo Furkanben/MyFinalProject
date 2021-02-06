b7938e11acdafc125223a4b6d96919d518d54579
Ben Furkan <furkan.bolatkale@hotmail.com>
2021-02-07 01:26:26 +0300

    7.2.21 - Notes and codes for Day 8 added.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint
+    //class: reference type
+    //IEntity: May be IEntity or an object that implements IEntity
+    //new(): Must be able to be instantiated (IEntity itself cannot be instantiated)
+    public interface IEntityRepository<T> where T:class,IEntity, new()
+    {        
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T product);
+    }
+}
